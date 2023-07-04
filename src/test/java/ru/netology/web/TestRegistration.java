1f1bb6489067ac12ca38ac4674fd755eafe441c6
Aleksandr Petrenko <aleksandr_petrenko62@mail.ru>
2023-07-04 20:28:15 +0300

    true test

--- src/test/java/ru/netology/web/TestRegistration.java
+++ src/test/java/ru/netology/web/TestRegistration.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestRegistration {
 
     private String generateData(int addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DataTimeFormatter.ofPattern(pattern));
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
     @Test
     void cardDeliveryValidValues() {
@@ -28,7 +28,7 @@ class TestRegistration {
         $$(".menu-item__control").findBy(text("Рязань")).click();
         String currentDate = generateData(4,"dd.MM.yyyy");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        //$("[data-test-id=date] input").sendKeys("currentDate");
+        $("[data-test-id=date] input").sendKeys(currentDate);
         $("[data-test-id=name] input").setValue("Александр");
         $("[data-test-id=phone] input").setValue("+78000000000");
         $("[data-test-id=agreement]").click();
