f8ef2e15b8b0222cedbf4221e4f44bc77e2b8f4d
Aleksandr Petrenko <aleksandr_petrenko62@mail.ru>
2023-07-04 19:39:00 +0300

    initial commit

---
+++ src/test/java/ru/netology/web/TestRegistration.java
@@ -0,0 +1,46 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.cssValue;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class TestRegistration {
+
+    private String generateData(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DataTimeFormatter.ofPattern(pattern));
+    }
+    @Test
+    void cardDeliveryValidValues() {
+        open("http://localhost:9999/");
+        //SelenideElement form = $("method=post");
+        $("[data-test-id=city] input").setValue("Ря");
+        $$(".menu-item__control").findBy(text("Рязань")).click();
+        String currentDate = generateData(4,"dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        //$("[data-test-id=date] input").sendKeys("currentDate");
+        $("[data-test-id=name] input").setValue("Александр");
+        $("[data-test-id=phone] input").setValue("+78000000000");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+
+
+
+    }
+}
+
+
+//data-day="1693342800000"
